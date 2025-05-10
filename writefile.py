d40dd702d1f8848ba8ed3d00f74d15c28e6988ed
Luu Duc Hai <hailuuduc.dev@gmail.com>
2025-05-10 18:07:24 +0700

    fix writefile

--- writefile.py
+++ writefile.py
@@ -1,7 +1,7 @@
 def write_file(file, message):
     try:
-        with open('file', 'a') as file:
+        with open(file, 'a') as file:
             file.write(f"{message}\n")
     except FileNotFoundError:
-        with open('file', 'w') as file:
+        with open(file, 'w') as file:
             file.write(f"{message}\n")
